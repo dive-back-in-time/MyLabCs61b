bc1c1b20e5e7e12147a730d65e05942efd3cbad0
dive-back-in-time <1531438238@qq.com>
2024-09-27 22:00:37 +0800

    Refresh

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -3,44 +3,48 @@ import java.util.List;
 import java.util.Map;
 
 public class MapExercises {
-    /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
+    /**
+     * Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        Map<Character , Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         char ch = 'a';
         for (int i = 1; i < 27; i++) {
-            map.put(ch,i);
+            map.put(ch, i);
             ch++;
         }
         return map;
     }
 
-    /** Returns a map from the integers in the list to their squares. For example, if the input list
-     *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
+    /**
+     * Returns a map from the integers in the list to their squares. For example, if the input list
+     * is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
         int size = nums.size();
         Map<Integer, Integer> map = new HashMap<>(size);
-        for(int num : nums){
-            map.put(num,num*num);
+        for (int num : nums) {
+            map.put(num, num * num);
         }
 
         return map;
     }
 
-    /** Returns a map of the counts of all words that appear in a list of words. */
+    /**
+     * Returns a map of the counts of all words that appear in a list of words.
+     */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map =   new HashMap<>();
         //Input the word into map
-        for(String word : words){
-            if(map.containsKey(word)){
-                map.put(word,map.get(word)+1);
-            }else{
-                map.put(word,1);
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
             }
         }
 
--- proj0/src/game2048rendering/Side.java
+++ proj0/src/game2048rendering/Side.java
@@ -41,7 +41,7 @@ public enum Side {
     }
 
     /** Return the standard x-coordinate for square (x, y) on a board
-     *  of size SIZE oriented with this Side on top. */
+     *  of size SIZE oriented with this Side on top.*/
     int x(int x, int y, int size) {
         return _col0 * (size - 1) + x * _drow + y * _dcol;
     }
