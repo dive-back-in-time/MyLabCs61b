7b49d9427a4afa6638cae9cbe34d305a6eabf10a
dive-back-in-time <1531438238@qq.com>
2024-09-29 23:44:55 +0800

    More tricky after compiling the resizedown. wuwuwuwuwu......

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -9,42 +9,42 @@ public class ArrayDeque61B<T> implements Deque61B {
     private int nextFirst;
     private int nextLast;
 
-    public ArrayDeque61B(){
+    public ArrayDeque61B() {
         maxSize = 8;
-        items = (T[])new Object[maxSize];
+        items = (T[]) new Object[maxSize];
         size = 0;
         nextFirst = 1;
         nextLast = 2;
 
     }
 
-    public void resizeUp(){
+    public void resizeUp() {
         int originmax = maxSize;
         maxSize = 2 * maxSize;
-        T[] newItems = (T[])new Object[maxSize];
+        T[] newItems = (T[]) new Object[maxSize];
 
         //Put the nextFirst and nextLast to the right place
         nextFirst = nextFirst + originmax;
 
         //Put the original array in new order
-        if (items[0] == null){
+        if (items[0] == null) {
             for (int i = 1; i < originmax; i++) {
                 newItems[i + originmax] = items[i];
             }
-        } else if (items[originmax - 1] == null){
+        } else if (items[originmax - 1] == null) {
             for (int i = 0; i < originmax - 1; i++) {
                 newItems[i] = items[i];
             }
         } else {
             for (int i = 0; i < originmax - 1; i++) {
-                if (items[i] == null){
+                if (items[i] == null) {
                     break;
                 }
                 newItems[i] = items[i];
             }
 
-            for (int i = originmax - 1; i > 0 ; i--) {
-                if (items[i] == null){
+            for (int i = originmax - 1; i > 0; i--) {
+                if (items[i] == null) {
                     break;
                 }
                 newItems[i + originmax] = items[i];
@@ -54,27 +54,50 @@ public class ArrayDeque61B<T> implements Deque61B {
 
     }
 
+    public void reSizeDown() {
+        int originMaxSize = maxSize;
+        maxSize = maxSize / 2;
+        T[] newItems = (T[]) new Object[maxSize];
+
+        //Based on nextFirst, arrange the values upon new-sizedown-array
+        int index = Math.floorMod(nextFirst + 1, originMaxSize);
+        while (items[index] != null) {
+            if (index >= maxSize) {
+                newItems[index - maxSize] = items[index];
+                index = Math.floorMod(index + 1, originMaxSize);
+            } else {
+                newItems[index] = items[index];
+                index = Math.floorMod(index + 1, originMaxSize);
+            }
+        }
+
+        nextFirst = Math.floorMod(nextFirst, maxSize);
+        nextLast = Math.floorMod(nextLast, maxSize);
+
+        items = newItems;
+    }
+
 
     @Override
     public void addFirst(Object x) {
-        if (size == maxSize - 1){
+        if (size == maxSize - 1) {
             resizeUp();
         }
-        items[nextFirst] = (T)x;
-        nextFirst = Math.floorMod(nextFirst - 1,maxSize);
+        items[nextFirst] = (T) x;
+        nextFirst = Math.floorMod(nextFirst - 1, maxSize);
         size++;
     }
 
     @Override
     public void addLast(Object x) {
-        if (size == maxSize - 1){
+        if (size == maxSize - 1) {
             resizeUp();
         }
 
-        items[nextLast] = (T)x;
+        items[nextLast] = (T) x;
 
 
-        nextLast = Math.floorMod(nextLast + 1,maxSize);
+        nextLast = Math.floorMod(nextLast + 1, maxSize);
         size++;
 
     }
@@ -82,15 +105,15 @@ public class ArrayDeque61B<T> implements Deque61B {
     @Override
     public List toList() {
         List<T> l = new ArrayList<>();
-        for(T item : items){
+        for (T item : items) {
             l.add(item);
         }
-        return l ;
+        return l;
     }
 
     @Override
     public boolean isEmpty() {
-        if (size == 0){
+        if (size == 0) {
             return true;
         }
         return false;
@@ -103,15 +126,32 @@ public class ArrayDeque61B<T> implements Deque61B {
 
     @Override
     public Object removeFirst() {
-        T t = (T)get(Math.floorMod(nextFirst + 1,maxSize));
+        if (size == 0) {
+            System.out.println("Array is empty now");
+            System.exit(1);
+        } else if (size <= maxSize / 4){
+            reSizeDown();
+        }
+
+        items[Math.floorMod(nextFirst + 1, maxSize)] = null;
+        T t = (T) get(Math.floorMod(nextFirst + 1, maxSize));
         nextFirst = Math.floorMod(nextFirst + 1, maxSize);
+        size--;
         return t;
     }
 
     @Override
     public Object removeLast() {
-        T t = (T)get(Math.floorMod(nextLast - 1,maxSize));
+        if (size == 0) {
+            System.out.println("Array is empty now");
+            System.exit(1);
+        } else if (size <= maxSize / 4){
+            reSizeDown();
+        }
+        items[Math.floorMod(nextLast - 1, maxSize)] = null;
+        T t = (T) get(Math.floorMod(nextLast - 1, maxSize));
         nextLast = Math.floorMod(nextLast - 1, maxSize);
+        size--;
         return t;
     }
 
@@ -128,18 +168,14 @@ public class ArrayDeque61B<T> implements Deque61B {
     public static void main(String[] args) {
         Deque61B<Integer> l = new ArrayDeque61B<>();
         l.addFirst(5);
-        l.addFirst(6);
-        l.addFirst(67);
-        l.addFirst(8);
-        l.addFirst(8);
-        l.addFirst(8);
-        l.addFirst(8);
-        l.addFirst(8);
-        l.addFirst(8);
-        l.addLast(18);
-        l.addLast(18);
+        l.addFirst(15);
+        l.addFirst(25);
+        l.addFirst(35);
+        l.removeFirst();
+        l.removeFirst();
         l.removeLast();
-        l.addLast(20);
+        l.removeLast();
+        l.removeLast();
 
         System.out.println(l.toList());
     }
